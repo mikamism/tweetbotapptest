e5070f020d2e2cbf9c302395f6d55371b03b34f3
mikamism <mikamism666@gmail.com>
2016-06-30 18:29:15 +0900

    log1

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
-var restify = require('restify');
-var builder = require('botbuilder');
- 
+var express = require('express');
 var port = process.env.PORT || 1337;
-
-console.log('test_start')
+var app = express();
  
-// Create bot and add dialogs
-var bot = new builder.BotConnectorBot({ appId: 'sample-tweet-bot', appSecret: '642d202a2f6540958e913cacd739da3d' });
-bot.add('/', function (session) {
-   session.send('Hello World'); 
+ console.log('test_start')
+ 
+app.get('/', function (req, res) {
+  res.send('Hello Express World!');
 });
  
-// Setup Restify Server
-var server = restify.createServer();
-server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
-server.listen(port, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port + '!');
 });
 
 console.log('test_end')
