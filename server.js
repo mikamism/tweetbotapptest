e7684249e8511b7e61d4016205deaa712fbcb28e
mikamism <mikamism666@gmail.com>
2016-06-30 19:49:31 +0900

    trail and error5

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 var restify = require('restify');
 var builder = require('botbuilder');
- 
-var port = process.env.PORT || 1337;
- 
+
 // Create bot and add dialogs
-var bot = new builder.BotConnectorBot({ botId: 'sample-tweet-bot', appSecret: '642d202a2f6540958e913cacd739da3d' });
+var bot = new builder.BotConnectorBot({ appId: 'sample-tweet-bot', appSecret: '642d202a2f6540958e913cacd739da3d' });
 bot.add('/', function (session) {
    session.send('Hello World'); 
 });
- 
+
 // Setup Restify Server
 var server = restify.createServer();
 server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
-server.listen(port, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+server.listen(process.env.port || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url); 
 });
