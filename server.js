a3313c8f69b9f7de2874904b1cb19cb0e42aa87f
mikamism <mikamism666@gmail.com>
2016-06-30 19:04:57 +0900

    trail and error3

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-var express = require('express');
+var restify = require('restify');
+var builder = require('botbuilder');
+ 
 var port = process.env.PORT || 1337;
-var app = express();
  
-
- 
-app.get('/', function (req, res) {
-     console.log('test_start')
-  res.send('Hello Express World!');
-  console.log('test_end')
+// Create bot and add dialogs
+var bot = new builder.BotConnectorBot({ appId: 'sample-tweet-bot', appSecret: '642d202a2f6540958e913cacd739da3d' });
+bot.add('/', function (session) {
+   session.send('Hello World'); 
 });
  
-app.listen(port, function () {
-  console.log('Example app listening on port ' + port + '!');
+// Setup Restify Server
+var server = restify.createServer();
+server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+server.listen(port, function () {
+   console.log('%s listening to %s', server.name, server.url); 
 });
-
