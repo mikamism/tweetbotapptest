71741795e951d021b3e426e280037af98b1ac02b
mikamism <mikamism666@gmail.com>
2016-06-30 19:34:31 +0900

    trail and error4

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var builder = require('botbuilder');
 var port = process.env.PORT || 1337;
  
 // Create bot and add dialogs
-var bot = new builder.BotConnectorBot({ appId: 'sample-tweet-bot', appSecret: '642d202a2f6540958e913cacd739da3d' });
+var bot = new builder.BotConnectorBot({ botId: 'sample-tweet-bot', appSecret: '642d202a2f6540958e913cacd739da3d' });
 bot.add('/', function (session) {
    session.send('Hello World'); 
 });
