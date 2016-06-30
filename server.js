9b18086c97398d42b864df17b2027f8f536cb361
mikamism <mikamism666@gmail.com>
2016-06-30 18:12:12 +0900

    trial and error2

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var restify = require('restify');
 var builder = require('botbuilder');
  
 var port = process.env.PORT || 1337;
+
+console.log('test_start')
  
 // Create bot and add dialogs
 var bot = new builder.BotConnectorBot({ appId: 'sample-tweet-bot', appSecret: '642d202a2f6540958e913cacd739da3d' });
@@ -15,3 +17,5 @@ server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
 server.listen(port, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
+
+console.log('test_end')
