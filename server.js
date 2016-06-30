4a4245ff567aeffc3963f543e83e167061735569
mikamism <mikamism666@gmail.com>
2016-06-30 18:31:50 +0900

    log2

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ var express = require('express');
 var port = process.env.PORT || 1337;
 var app = express();
  
- console.log('test_start')
+
  
 app.get('/', function (req, res) {
+     console.log('test_start')
   res.send('Hello Express World!');
+  console.log('test_end')
 });
  
 app.listen(port, function () {
   console.log('Example app listening on port ' + port + '!');
 });
 
-console.log('test_end')
