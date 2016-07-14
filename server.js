340cafec4a6301241ccd390649ca8ee4b1fbc871
mikamism <mikamism666@gmail.com>
2016-07-14 18:46:00 +0900

    db_access1

--- server.js
+++ server.js
@@ -1,125 +1,83 @@
 var restify = require('restify');
 var builder = require('botbuilder');
-var tmpdata = {};
 
 var bot = new builder.BotConnectorBot(
   { appId: 'sample-tweet-bot',
     appSecret: '642d202a2f6540958e913cacd739da3d' });
 
-bot.use(function(session, next) {
-  if (!session.userData.addrbook)
-    session.userData.addrbook = [];
-  next();
-});
+bot.add('/', new builder.Dialog()
+    // 大文字小文字でも正規表現でひとまとめとする
+    .matches('^(exile|EXILE)', builder.DialogAction.beginDialog('/exile'))
+    .matches('^(test|TEST)', builder.DialogAction.beginDialog('/test'))
+    .matches('^func', showFuncMessage)
+    .onDefault(function (session) {
+        var msg = 'This is a test for TweetBot of SQLServer!!';
+        session.send('Hello, I am Test bot! ' + msg);
+    }));
 
-bot.add('/', new builder.CommandDialog()
-  .matches('^(regist|add)',  builder.DialogAction.beginDialog('/regist'))
-  .matches('^(find|search)', builder.DialogAction.beginDialog('/find'))
-  .matches('^(exile|search)', builder.DialogAction.beginDialog('/exile'))
-  .matches('^list', showList)
-  .onDefault(function (session) {
-    var msg = 'you have ' + session.userData.addrbook.length + ' data.';
-    session.send('Hello, I am address book bot. ' + msg);
-  }));
-
-function showList(session) {
-  var tmp = session.userData.addrbook.map(
-    current => 'name: ' + current.name + ' tel: ' + current.tel);
+function showFuncMessage(session) {
   session.send(tmp.join(', '));
 }
 
-bot.add('/regist', [
-  function(session) { 
-    builder.Prompts.text(session, 'what name do you want to add ?');
-  },
-  function(session, results) {
-    tmpdata.name = results.response;
-    builder.Prompts.text(session, 'telephone number? ');
-  },
-  function(session, results) {
-    tmpdata.tel = results.response;
-    session.userData.addrbook.push(tmpdata);
-    session.send('registerd!');
-    session.endDialog();
-  }
+// bot振り分け後の処理
+bot.add('/test', [
+    function (session) {
+        session.send('You said Test!!');
+    },
 ]);
 
-bot.add('/find', [
-  function(session) {
-    builder.Prompts.text(session, 'find? ');
-  },
-  function(session, results) {
-    var target = results.response;
-    var res = session.userData.addrbook.filter(
-      item => item.name == target ? true : false
-    );
-    if (res[0]) {
-      session.send("name: " + res[0].name + " tel: " + res[0].tel);
-    } else {
-      session.send('no such data');
-    }
-    session.endDialog();
-  }, 
-]);
-
-// exileと話しかけた場合の処理
 bot.add('/exile', [
-  function(session) {
+     function (session) {
+        // コネクションの作成
+        var Connection = require('tedious').Connection;
+        var config = {
+            userName: 'socialadmin',
+            password: 'ufeuQ7sPu2',
+            server: 'socialtestdb.database.windows.net',
+            // Azure上のDBの場合は必須
+            options: { encrypt: true, database: 'socialtestdb' }
+        };
 
-    // コネクションの作成
-    var Connection = require('tedious').Connection;
-    var config = {
-      userName: 'socialtestdb@socialtestdb.database.windows.net',
-      password: 'ufeuQ7sPu2',
-      server: 'socialtestdb.database.windows.net',
-      // Azure上のDBの場合は必須
-      options: {encrypt: true, database: 'AdventureWorks'}
-    };
+        var connection = new Connection(config);
+        connection.on('connect', function (err) {
+            // If no error, then good to proceed. 
+            console.log("Connected");
+            executeStatement(session, connection);
+        });
+      },
 
-builder.Prompts.text(session, 'kiminimuchu2');
+]);
 
-    var connection = new Connection(config);
-    connection.on('connect', function(err) {
-      // If no error, then good to proceed. 
-      console.log("Connected");
-      builder.Prompts.text(session, 'connection!!');
-      executeStatement(session);
-    });
-
-builder.Prompts.text(session, 'kiminimuchu3');
-    //session.send("kiminimuchu");
-    //session.endDialog();
-    
+function executeStatement(session, connection) {
     var Request = require('tedious').Request;
     var TYPES = require('tedious').TYPES;
-  },
-  function executeStatement(session) {
+
     // クエリの作成
-    request = new Request("SELECT a.username,MAX(a.follower) FROM dbo.TwitteruserFollowerList a WHERE a.username like 'EXILE%' GROUP BY a.username;", function(err) {
-      if (err) { 
-        console.log(err);
-      }
+    var request = new Request("SELECT a.username,MAX(a.follower) FROM dbo.TwitteruserFollowerList a WHERE a.username like 'EXILE%' GROUP BY a.username;", function (err) {
+        if (err) {
+            console.log(err);
+        }
     });
     var result = "";
-    request.on('row', function(columns) {
-      columns.forEach(function(column) {
-        if (column.value === null) {
-          console.log('NULL');
-        } else {
-          result+= column.value + " ";
-        }
-      });
-      console.log(result);
-      result ="";
+    request.on('row', function (columns) {
+        columns.forEach(function (column) {
+            if (column.value === null) {
+                console.log('NULL');
+            } else {
+                result += column.value + " ";
+            }
+        });
+        //console.log(result);
+        builder.Prompts.text(session, result);
+        result = "";
     });
 
-    request.on('done', function(rowCount, more) {
-      console.log(rowCount + ' rows returned');
+    request.on('done', function (rowCount, more) {
+        console.log(rowCount + ' rows returned');
     });
     connection.execSql(request);
     //builder.Prompts.text(session, result);
-  }
-]);
+}
 
 var server = restify.createServer();
 server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
