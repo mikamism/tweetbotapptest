b7aceab47c3382ab41844684abbec154c76fb084
mikamism <mikamism666@gmail.com>
2016-09-05 18:54:44 +0900

    add urlencodeing2

--- server.js
+++ server.js
@@ -288,9 +288,9 @@ bot.add('/twittertrend', [
     // DB接続
     connection.on('connect', function (err) {
       var sql = "SELECT TOP 20 "
-                + "CONVERT(varchar(5),ROW_NUMBER() OVER(ORDER BY SUM(a.score) DESC)) + ' ： [' + a.word + '](" + encodeURIComponent("https://twitter.com/search?q= + REPLACE(a.word,'#','%23') + '&src=tren)'") + " as row "
-                + ",'[ [*Google*](" + encodeURIComponent("https://www.google.co.jp/search?q=' + REPLACE(a.word,'#','') + ')") + "]' google "
-                + ",'[ [*Trend*](" + encodeURIComponent("https://www.google.co.jp/trends/explore?date=now%201-d&geo=JP&q=' + REPLACE(a.word,'#','') + ')") + "]' trend "
+                + "CONVERT(varchar(5),ROW_NUMBER() OVER(ORDER BY SUM(a.score) DESC)) + ' ： [' + a.word + '](" + encodeURI("https://twitter.com/search?q= + REPLACE(a.word,'#','%23') + '&src=tren)'") + " as row "
+                + ",'[ [*Google*](" + encodeURI("https://www.google.co.jp/search?q=' + REPLACE(a.word,'#','') + ')") + "]' google "
+                + ",'[ [*Trend*](" + encodeURI("https://www.google.co.jp/trends/explore?date=now%201-d&geo=JP&q=' + REPLACE(a.word,'#','') + ')") + "]' trend "
                 + ",dbo.funcExistTwitterTrendMaster(a.word) + ':' newflg "
                 + "FROM dbo.T_TwitterTrendWordsHour a "
                 + "WHERE a.timeSum >= CONVERT(DATETIME, CONVERT(varchar(13), DATEADD(hour, -24, dbo.Now()), 120)+':00') "
